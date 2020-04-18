f03afcda35cb23f13be95d73e294c6aecd8f3638
orkeren150 <orkeren150@walla.com>
2020-04-18 17:37:49 +0300

    Update attendance.py

---
+++ stats/attendance.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from data import games
+
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+attendance.columns = ['year', 'attendance']
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
+
+attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
+plt.xlabel('Year')
+plt.ylabel('Attendance')
+plt.axhline(label=attendance['Attendance'].mean(), linestyle = '-', color = 'green')
+plt.show()
